f3277d2a417c49a55b4a9c3151326fdec9db6269
zhuorui li <zhuorui.li.gm@gmail.com>
2026-01-02 16:17:14 +0800

    优化

--- svga/build.gradle.kts
+++ svga/build.gradle.kts
@@ -13,15 +13,7 @@ android {
     defaultConfig {
         minSdk = 21
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        // 关键：混淆规则，确保库在混淆后仍能正常运行
         consumerProguardFiles("consumer-rules.pro")
-    }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
-        }
     }
 
     buildTypes {
@@ -39,19 +31,6 @@ android {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.github.TroyLi-Dev"
-            artifactId = "compose-svga"
-            version = project.version as String? ?: "1.0.0"
-            afterEvaluate {
-                from(components["release"])
-            }
-        }
-    }
-}
-
 kotlin {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_11)
@@ -59,16 +38,14 @@ kotlin {
 }
 
 dependencies {
-    // 基础核心
     implementation(libs.androidx.core.ktx)
-
-    // Compose 核心：仅包含绘制和基础组件，不包含 Material 样式
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.foundation) // 用于 Box, 基础动画
-    implementation(libs.androidx.compose.runtime)    // 用于 State, LaunchedEffect
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.runtime)
 
     // SVGA 核心依赖
-    implementation(libs.wire.runtime) // Protobuf 解析
-    implementation(libs.okio) // 流式解码基础
+    implementation(libs.wire.runtime)
+    implementation(libs.okio)
+
 }
